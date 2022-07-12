e884204f11da13f1010c2a58912beab2a9e49c9e
Xusniddin <narkulof@gmail.com>
2022-07-12 17:46:01 +0500

    setup firebase, header component style completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 // import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <BrowserRouter>
+         <div className="App">
+            <Header /> 
+            <Routes>
+               <Route path="/" element={<Home />} />
+            </Routes>
+         </div>
+      </BrowserRouter>
+   );
 }
 
 export default App;
---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyCEPjwlBPBsCUHi6s4s-4qXMJ81XhsDgcA",
+   authDomain: "clone-e9342.firebaseapp.com",
+   projectId: "clone-e9342",
+   storageBucket: "clone-e9342.appspot.com",
+   messagingSenderId: "502999367259",
+   appId: "1:502999367259:web:d4ff541e9210bf95ae497e",
+};
+
+const firebasApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebasApp.firestore();
+export const auth = firebase.auth();
